96430da637d4ff61dc4033a5ef34c35ea413740a
HWHKL <76677612+HWHKL@users.noreply.github.com>
2021-01-25 00:53:15 +0800

    更新 feedback.py

--- hoshino/modules/botmanage/feedback.py
+++ hoshino/modules/botmanage/feedback.py
@@ -3,20 +3,20 @@ from hoshino import Service, priv
 from hoshino.typing import CQEvent
 from hoshino.util import DailyNumberLimiter
 
-sv = Service('_feedback_', manage_priv=priv.SUPERUSER, help_='[来杯咖啡] 后接反馈内容 联系维护组')
+sv = Service('_feedback_', manage_priv=priv.SUPERUSER, help_='[反馈] 后接反馈内容 联系维护组')
 
 _max = 3
 lmt = DailyNumberLimiter(_max)
-EXCEED_NOTICE = f'您今天已经喝过{_max}杯了，请明早5点后再来！'
+EXCEED_NOTICE = f'您今天已经反馈过{_max}次了，请明早5点后再来！'
 
-@sv.on_prefix('来杯咖啡')
+@sv.on_prefix('反馈')
 async def feedback(bot, ev: CQEvent):
     uid = ev.user_id
     if not lmt.check(uid):
         await bot.finish(ev, EXCEED_NOTICE, at_sender=True)
     text = ev.message
     if not text:
-        await bot.send(ev, f"请发送来杯咖啡+您要反馈的内容~", at_sender=True)
+        await bot.send(ev, f"请发送反馈+您要反馈的内容~", at_sender=True)
     else:
         coffee = hoshino.config.SUPERUSERS[0]
         await bot.send_private_msg(self_id=ev.self_id, user_id=coffee, message=f'Q{uid}@群{ev.group_id}\n{text}')

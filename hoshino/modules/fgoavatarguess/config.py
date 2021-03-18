f549a348078685cbb19af21d42b25fedac0fa76c
HWHKL <howardkelvin174@gmail.com>
2021-03-18 16:23:45 +0800

    1

--- hoshino/modules/fgoavatarguess/config.py
+++
@@ -1,8 +0,0 @@
-ICON_RES = 'fgo_avatar'  # res/img下的目录名
-ICON_PREFIX = 'Servant'  # 图片编号前缀
-ID_OFFSET = -1000  # 图片编号与data编号的差值
-ICON_EXT = 'jpg'  # 图片格式
-
-START_COMMAND = ('猜从者', 'fgo猜头像')  # 游戏开始命令
-RANK_COMMAND = ('fgo猜头像排行榜', '猜从者排行榜')  # 排行榜命令
-DB_PATH = r'~/.hoshino/winning_counter.db'  # 排行榜数据存放位置
